e47236a928964fad7eaad0a971f31291e890b0b0
Brandi <pvtpyro@yahoo.com>
2025-05-14 19:49:10 -0700

    add player, allow rotation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from player import Player
 import pygame
 from constants import *
 
@@ -8,13 +9,20 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 activate virtual environment with `source venv/bin/activate` in WSL
 
 run program with `python3 main.py`
+
+## Delta Time
+
+In math, the Greek letter delta (Δ) is often used to represent a change in a value. In game development, we use "delta time" to represent the amount of time that has passed since the last frame was drawn. This value is useful to decouple the game's speed from the speed it's being drawn to the screen.
+
+If your computer speeds up, the asteroids shouldn't also speed up. Conversely, if your computer slows down, the asteroids shouldn't also slow down: they would just move less smoothly.
